cbf0bcd5631f6047b31cdcedcaaa4503360db3c7
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-06-10 16:06:37 -0400

    CRM web app.

--- crm_real.rb
+++ crm_real.rb
@@ -1,5 +1,9 @@
 require "sinatra"
 require_relative "contact"
+require_relative "rolodex"
+
+@@rolodex = Rolodex.new
+# To enable access to Rolodex from each action in Sinatra.
 
 get "/" do
 	@crm_app_name = "My CRM"
@@ -8,12 +12,12 @@ end
 
 # View all contacts.
 get "/contacts" do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+	# @contacts = []
+	# @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end
 
 # Add a new contact.
 get "/contacts/new" do
-	 # erb :new
+	 erb :new_contact
 end
