59ff2479ed119240935fb4bf490300a878a72a27
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-06-10 15:24:50 -0400

    CRM web app.

--- crm_real.rb
+++ crm_real.rb
@@ -1,5 +1,17 @@
 require "sinatra"
+require_relative "contact"
 
 get "/" do
-	"Main menu"
+	@crm_app_name = "My CRM"
+	erb :index
+end
+
+# View all contacts.
+get "/contacts" do
+	erb :contacts
+end
+
+# Add a new contact.
+get "/contacts/new" do
+	 # erb :new
 end
